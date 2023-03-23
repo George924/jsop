7419dd2d1b53dfd0d0f7f2705cefdd484085919a
Frontend JavaScript <frontendjavascript@C3-06.local>
2023-03-23 20:46:57 +0200

    curs

--- curs1/js/student.js
+++ curs1/js/student.js
@@ -1,5 +1,23 @@
  class Student {
-    constructor(id, nume ,class, )
+    constructor(id, nume , varsta, nota1, nota2 )
+    {
+    this.id=id
+    this.nume=nume
+    this.varsta=varsta
+    this.nota1=9
+    this.nota2=10
+}
+numeleStudentului(numeNou){
+    console.log('salut'(numeNou));
+
+}
+medieElev(medieNoua) {
+    media=nota1+nota2
+
+}
+
+
+
  }
 
   
