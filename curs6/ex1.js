3594daf2bf5e2591b72d29259ca3a13512609c05
Frontend JavaScript <frontendjavascript@C3-06.local>
2023-04-25 20:43:09 +0300

    curs6

---
+++ curs6/ex1.js
@@ -0,0 +1,11 @@
+// 1. definim o functie
+let adunare = (a, b) => {  
+    return a + b;
+}
+
+// 2. apelam functia
+//console.log("Suma dintre 24 si 46 este", adunare(24, 46));
+ let suma = adunare(5,6);
+ 
+
+// 3. in terminal rulam comanda: node  sau nodemon ex1.js daca modulul nodemon este instalat
